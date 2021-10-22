eddd0990334d8cc7b08c9cb7f82db97956c575f7
Nadia <music-hood@yandex.ru>
2021-10-22 19:52:47 +0500

    the first changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { AddFormComponent } from './add-form/add-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    AddFormComponent,
+    FooterComponent,
+    TasksListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
